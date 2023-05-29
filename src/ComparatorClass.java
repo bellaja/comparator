718327d7a140429c975bba78ef501d47153c5f96
bellaja <bellaja185@gmail.com>
2023-05-29 20:32:06 +0300

    1

---
+++ src/ComparatorClass.java
@@ -0,0 +1,8 @@
+import java.util.Comparator;
+
+public class ComparatorClass implements Comparator<Person> {
+    @Override
+    public int compare(Person o1, Person o2) {
+        return Integer.compare(o1.getSurname().length(), o2.getSurname().length());
+    }
+}
